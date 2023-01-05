b30f549223ab0f424139a5a3cd7efb9d8d5fbea8
arion-p <pkouvar-other@yahoo.gr>
2023-01-06 01:09:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-r7yb5e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-r7yb5e)
---
+++ src/parent/parent.component.ts
@@ -0,0 +1,24 @@
+import {
+  Component,
+  ContentChild,
+  Input,
+  OnInit,
+  TemplateRef,
+} from '@angular/core';
+import { NgTestTmplDirective } from '../ng-test-tmpl.directive';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css'],
+})
+export class ParentComponent implements OnInit {
+  @Input() item: any;
+
+  @ContentChild(NgTestTmplDirective, { read: TemplateRef })
+  template: TemplateRef<any>;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
